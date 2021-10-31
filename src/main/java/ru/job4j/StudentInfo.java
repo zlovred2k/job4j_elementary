108023f07a9e5649dc5b3536f98fc4ab03381b8b
zlovred2k <zlovred2k@gmail.com>
2021-10-31 23:51:14 +0300

    Разбор первой программы. [#159033 #222702]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Alex Muravev");
+        System.out.println("01.01.2021");
+    }
+}
