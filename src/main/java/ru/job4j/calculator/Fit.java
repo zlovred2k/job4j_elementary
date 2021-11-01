29cf55ff6c59d6bb6639a29c561c4ece723e8053
zlovred2k <zlovred2k@gmail.com>
2021-11-01 18:53:27 +0300

    Идеальный вес [#41584]

---
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -0,0 +1,20 @@
+package ru.job4j.calculator;
+
+public class Fit {
+
+    public static double manWeight(short height) {
+        return (height - 100) * 1.15;
+    }
+
+    public static double womanWeight(short height) {
+        return (height - 110) + 1.15;
+    }
+
+    public static void main(String[] args) {
+        short manHeight = 180;
+        double manWeight = manWeight(manHeight);
+        System.out.println("Man weight: " + manWeight);
+
+    }
+
+}
