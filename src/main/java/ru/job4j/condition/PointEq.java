7f5cebc3b35e454bd2cede31f283d341e1df4e97
zlovred2k <zlovred2k@gmail.com>
2021-11-04 10:37:00 +0300

    if c return [#333785]

--- src/main/java/ru/job4j/condition/PointEq.java
+++ src/main/java/ru/job4j/condition/PointEq.java
@@ -8,7 +8,7 @@ public class PointEq {
 
     public static void main(String[] args) {
         System.out.println(eq(1, 2, 1, 2));
-        System.out.println(eq(10, 10, 10, 11));
+        System.out.println(eq(10, 10, 10, 12));
 
     }
 }
