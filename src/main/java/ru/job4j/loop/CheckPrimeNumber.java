4e253760d351ffbc3bd91d912396dd6705502562
zlovred2k <zlovred2k@gmail.com>
2021-11-05 17:01:46 +0300

    Оператор прерывания цикла - break

---
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        boolean prime = true;
+        if (number == 0 || number == 1) {
+            prime = false;
+        }
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                prime = false;
+                break;
+            }
+        }
+        return prime;
+    }
+}
---
+++ src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CheckPrimeNumberTest {
+
+    @Test
+    public void when5() {
+        int number = 5;
+        boolean result = CheckPrimeNumber.check(number);
+        assertTrue(result);
+    }
+
+    @Test
+    public void when4() {
+        int number = 4;
+        boolean result = CheckPrimeNumber.check(number);
+        assertFalse(result);
+    }
+
+    @Test
+    public void when1() {
+        int number = 1;
+        boolean result = CheckPrimeNumber.check(number);
+        assertFalse(result);
+    }
+}
